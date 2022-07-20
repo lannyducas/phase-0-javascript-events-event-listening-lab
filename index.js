14b9f1b8be7aedaaff10f0e867c1953154a14578
Danny <lannyducas@gmail.com>
2022-07-20 16:46:35 -0400

    commit tree 5dec8abf763da8a19c8fb3da1ef0d00f711be5ce

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+/*const input = document.querySelector('button');
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
+*/
+//const input = document.querySelector('button');
+
+
+function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+}
+
+
+
+
+
+
